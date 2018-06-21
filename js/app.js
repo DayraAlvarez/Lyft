53f462a135fc2aab59e80dcd9476d607f86cba74
DayraAlvarez <dayra.g.alvarez@gmail.com>
2018-06-21 14:09:44 -0500

    Fixed intro background

--- js/app.js
+++ js/app.js
@@ -1,4 +1,37 @@
+//alert ('hola');
+//click event listener = hide y show en jquery
+
+//1. When choosing flag, prefix shows
+//2. Enable button until writing down number
+//3. Modal with sign up code lab-xxx
+//4. Enter code, enable button
+//5. Sign up, enble button when check terms
+//6. Modal, sign up succes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 $( document ).ready(function() {
+
+    $('.modal').modal();
     $('.setUp').hide();
     setTimeout(function(){
         $('.setUp').show();
@@ -7,5 +40,3 @@ $( document ).ready(function() {
     }, 3000);
     $('select').formSelect();
 });
-
-//click event listener = hide y show en jquery
