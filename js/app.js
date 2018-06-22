fdc4320bb5927bfb27cd411f60ae53dc2bed75be
DayraAlvarez <dayra.g.alvarez@gmail.com>
2018-06-22 04:04:09 -0500

    Disabled/enabled buttons, pending to disable/enable check box

--- js/app.js
+++ js/app.js
@@ -1,38 +1,3 @@
-//alert ('hola');
-//click event listener = hide y show en jquery
-
-//1. When choosing flag, prefix shows
-//2. Enable button until writing down number
-//3. Modal with sign up code lab-xxx
-//4. Enter code, enable button
-//5. Sign up, enble button when check terms
-//6. Modal, sign up succes
-function myFunction() {
-  var copyText = document.getElementById("myInput");
-  copyText.select();
-  document.execCommand("copy");
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 $( document ).ready(function() {
 
     $('.modal').modal();
@@ -42,4 +7,23 @@ $( document ).ready(function() {
         $('.intro').hide();
     }, 3000);
     $('select').formSelect();
+    //Disable, enable buttons
+    $('#searchInput').keyup(function(){
+        if($(this).val() == ''){
+          $('.enableOnInput').prop('disabled',true);
+      }else{
+          $('.enableOnInput').prop('disabled',false);
+      }
+    });
 });
+    //Disable, enable checkbox
+    //$('#checkThis').click(function(){
+        //if (this.checked) {
+         // $("input.group1").removeAttr("disabled");
+        //} else {
+        //  $("input.group1").attr("disabled", true);
+    //});
+//
+
+//<input type="text"  id="searchInput"/>
+//<a href="login.html"><input type="submit" value="Return to Log In"  id="submitBtn" class="enableOnInput" disabled=disabled/></a>
